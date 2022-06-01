5318df41c8bc4677ac18e35b83b72172390fcad3
maro5397 <lsj000424@gmail.com>
2022-06-01 22:51:18 +0900

    ✨ build: add style and signal, plot

--- app/main.py
+++ app/main.py
@@ -1,23 +1,31 @@
-import sys
+import sys, os
 import time
 import threading
+import psutil
 import cv2
 import numpy
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from src import jsonmodule as jm
+from src import signal
 
 
 class Main(QDialog):
     
     def __init__(self):
         super().__init__()
-        self.path = '../src/search.mp4'
         self.initUI()
-        self.setValueforWidget()
-        self.thread = threading.Thread(target=self.viewVideo, args=[])
-        self.thread.daemon = True
-        self.thread.start()
+        self.path = '../src/search.mp4'
+        self.string = "Test Text"
+        self.signalofprogress = signal.UpdateProgressBar()
+        self.signalofprogress.signal.connect(self.updateProgressBar)
+        self.signaloftextedit = signal.UpdateTextEdit()
+        self.signaloftextedit.signal.connect(self.updateTextEdit)
+        self.printText("init UI complete")
+        self.makeThread()
+        self.printText("start Thread")
 
     def initUI(self):
         self.main_layout = QVBoxLayout()
@@ -31,23 +39,48 @@ class Main(QDialog):
 
         self.layout_1 = QVBoxLayout()
         self.layout_2 = QVBoxLayout()
+        self.layout_2.setSpacing(20)
         self.layout_3 = QVBoxLayout()
         
+        self.font_1 = QFont("MS Reference Sans Serif", 16, QFont.Bold)
+        self.font_2 = QFont('Consolas', 16)
+        self.font_3 = QFont('Leelawadee UI', 16, QFont.Bold)
+        self.font_4 = QFont('Leelawadee UI', 14)
+        self.font_5 = QFont('Consolas', 12, QFont.Bold)
+        
         self.groupbox_1 = QGroupBox('Incoming Packet Content')
+        self.groupbox_1.setFont(self.font_1)
+        self.groupbox_1.setStyleSheet("QGroupBox:title {color: rgb(200, 0, 0); background-color: rgb(200, 220, 220); border: 1px solid black;}"
+                                      "QGroupBox {border: 1px solid black; background-color: rgb(255, 255, 255);}")
+        
         self.vbox_1 = QVBoxLayout()
         self.label_1 = QLabel('0', self)
+        self.label_1.setFont(self.font_2)
         self.vbox_1.addWidget(self.label_1)
         self.groupbox_1.setLayout(self.vbox_1)
         self.layout_2.addWidget(self.groupbox_1)
         
         self.groupbox_2 = QGroupBox('Most Similar File')
+        self.groupbox_2.setFont(self.font_1)
+        self.groupbox_2.setStyleSheet("QGroupBox:title {color: rgb(150, 200, 0); background-color: rgb(200, 220, 220); border: 1px solid black;}"
+                                      "QGroupBox {border: 1px solid black; background-color: rgb(255, 255, 255);}")
         self.hbox_1 = QHBoxLayout()
         self.hbox_2 = QHBoxLayout()
         self.vbox_2 = QVBoxLayout()
         self.label_2 = QLabel('0', self)
+        self.label_2.setAlignment(Qt.AlignLeft | Qt.AlignVCenter)
+        self.label_2.setFixedWidth(210)
+        self.label_2.setFont(self.font_2)
         self.label_3 = QLabel('File Name', self)
+        self.label_3.setFont(self.font_3)
         self.label_4 = QLabel('0', self)
+        self.label_4.setStyleSheet("QLabel {color: rgb(255, 0, 0);}")
+        self.label_4.setAlignment(Qt.AlignLeft | Qt.AlignVCenter)
+        self.label_4.setFixedWidth(210)
+        self.label_4.setFont(self.font_2)
         self.label_5 = QLabel('Similarity', self)
+        self.label_5.setFont(self.font_3)
+        self.label_5.setStyleSheet("QLabel {color: rgb(255, 0, 0);}")
         self.hbox_1.addWidget(self.label_3)
         self.hbox_1.addWidget(self.label_2)
         self.hbox_2.addWidget(self.label_5)
@@ -58,13 +91,44 @@ class Main(QDialog):
         self.layout_2.addWidget(self.groupbox_2)
         
         self.groupbox_3 = QGroupBox('Compute Status')
+        self.groupbox_3.setFont(self.font_1)
+        self.groupbox_3.setStyleSheet("QGroupBox:title {color: rgb(0, 130, 130); background-color: rgb(200, 220, 220); border: 1px solid black;}"
+                                      "QGroupBox {border: 1px solid black; background-color: rgb(255, 255, 255);}")
         self.hbox_3 = QHBoxLayout()
         self.hbox_4 = QHBoxLayout()
         self.vbox_3 = QVBoxLayout()
         self.progress_1 = QProgressBar(self)
-        self.label_6 = QLabel('CPU Speed', self)
+        self.progress_1.setFixedWidth(200)
+        self.progress_1.setStyleSheet("""
+                                    QProgressBar {
+                                    background-color: #C0C6CA;
+                                    height: 40px;
+                                    }
+                                    QProgressBar:chunk {
+                                    background: #7D94B0;
+                                    }
+                                    """)
+        self.progress_1.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+        self.label_6 = QLabel('CPU usage', self)
+        self.label_6.setStyleSheet("QLabel {margin-bottom: 6px;}")
+        self.label_6.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+        self.label_6.setFont(self.font_4)
         self.progress_2 = QProgressBar(self)
+        self.progress_2.setFixedWidth(200)
+        self.progress_2.setStyleSheet("""
+                                    QProgressBar {
+                                    background-color: #C0C6CA;
+                                    height: 40px;
+                                    }
+                                    QProgressBar:chunk {
+                                    background: #7D94B0;
+                                    }
+                                    """)
+        self.progress_2.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
         self.label_7 = QLabel('Memory usage', self)
+        self.label_7.setStyleSheet("QLabel {margin-bottom: 6px;}")
+        self.label_7.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+        self.label_7.setFont(self.font_4)
         self.hbox_3.addWidget(self.label_6)
         self.hbox_3.addWidget(self.progress_1)
         self.hbox_4.addWidget(self.label_7)
@@ -75,10 +139,13 @@ class Main(QDialog):
         self.layout_2.addWidget(self.groupbox_3)
         
         self.textedit = QTextEdit()
+        self.textedit.setStyleSheet("QTextEdit {background-color: rgb(0, 0, 0); color: rgb(0, 255, 0);}")
+        self.textedit.setFont(self.font_5)
         self.textedit.setReadOnly(True)
         self.layout_3.addWidget(self.textedit)
         
         self.label_8 = QLabel()
+        self.label_8.setAlignment(Qt.AlignCenter)
         self.layout_1.addWidget(self.label_8)
 
         self.frame_1.setLayout(self.layout_1)
@@ -101,14 +168,30 @@ class Main(QDialog):
         self.setLayout(self.main_layout)
         self.setWindowTitle('DarkWeb Monitoring System User View')
         self.showMaximized()
+        return
         
         
-    def setValueforWidget(self):
-        self.progress_1.setValue(80)
-        self.progress_2.setValue(50)
+    def makeThread(self):
+        self.videothread = threading.Thread(target=self.viewVideoFlow, args=[])
+        self.videothread.daemon = True
+        self.computethread = threading.Thread(target=self.setComputeValueFlow, args=[])
+        self.computethread.daemon = True
+        self.socketthread = threading.Thread(target=self.clientSocketFlow, args=[])
+        self.socketthread.daemon = True
+        self.videothread.start()
+        self.computethread.start()
+        self.socketthread.start()
+        return
         
         
-    def viewVideo(self):
+    def setComputeValueFlow(self):
+        while True:
+            self.signalofprogress.run()
+            time.sleep(1)
+        return
+        
+        
+    def viewVideoFlow(self):
         cap = cv2.VideoCapture(self.path)
         fps = cap.get(cv2.CAP_PROP_FPS)
         sleep_ms = int(numpy.round((1 / fps) * 500))
@@ -120,7 +203,7 @@ class Main(QDialog):
                 qimg = QImage(rgbimage.data, w, h, w * c, QImage.Format_RGB888)
                 pixmap = QPixmap(qimg)
                 
-                p = pixmap.scaled(1400, 700, Qt.IgnoreAspectRatio)
+                p = pixmap.scaled(1500, 800, Qt.IgnoreAspectRatio)
                 self.label_8.setPixmap(p)
                 self.label_8.update()
                 if cv2.waitKey(sleep_ms) == ord('q'):
@@ -131,8 +214,32 @@ class Main(QDialog):
                 sleep_ms = int(numpy.round((1 / fps) * 500))
         cap.release()
         cv2.destroyAllWindows()
+        return
+    
+    
+    def clientSocketFlow(self):
+        return
+    
+    
+    def printText(self, string):
+        self.string = string
+        self.signaloftextedit.run()
+        return
+        
 
-
+    @pyqtSlot()
+    def updateProgressBar(self):
+        cpu = int(psutil.cpu_percent())
+        memory = int(psutil.virtual_memory().percent)
+        self.progress_1.setValue(cpu)
+        self.progress_2.setValue(memory)
+    
+    @pyqtSlot()
+    def updateTextEdit(self):
+        self.textedit.append(self.string)
+        
+        
+        
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     main = Main()
