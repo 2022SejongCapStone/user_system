ec3cb070f2ddbe9642b5795612fe0c135463391c
maro5397 <lsj000424@gmail.com>
2022-06-01 22:54:44 +0900

    ✨ feat: add signal file for pyqt UI

---
+++ src/signal.py
@@ -0,0 +1,15 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+class UpdateProgressBar(QObject):
+    signal = pyqtSignal()
+
+    def run(self):
+        self.signal.emit()
+        
+class UpdateTextEdit(QObject):
+    signal = pyqtSignal()
+    
+    def run(self):
+        self.signal.emit()
